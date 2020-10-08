fd775dfa823edc9951b9838c4f31f81bfd6642ca
Shuyi Fang <66406517+Terence0101@users.noreply.github.com>
2020-10-08 15:40:08 +0800

    Update code.py

--- code.py
+++ code.py
@@ -7,8 +7,7 @@ print("Sample Input: yyyy/mm/dd USD 1000 | Stop by [Enter]")
 while 1:
     try:
         date,currency,money = map(str,input().split(' '))
-        params = {"all_year":date[0:4], "all_month":date[5:7],"all_day":date[8:10]}
-        res = requests.post(url="https://rate.bot.com.tw/xrt/all/{}-{}-{}".format(date[0:4],date[5:7],date[8:10]),data=params)
+        res = requests.get(url="https://rate.bot.com.tw/xrt/all/{}".format(date.replace('/','-')))
         doc = pq(res.text)
         for info in doc("tbody tr:nth-child(n)").items():
             if (info("td:nth-child(1).currency.phone-small-font > div > div.hidden-phone.print_show").text()[-4:-1]) == currency:
