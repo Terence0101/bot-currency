f3985c37172770877d830993c7638c02664b6576
Shuyi Fang <66406517+Terence0101@users.noreply.github.com>
2020-10-08 11:28:41 +0800

    Update code.py

--- code.py
+++ code.py
@@ -2,6 +2,8 @@ import requests
 from pyquery import PyQuery as pq
 
 resultNTD = 0
+print("Sample Input: yyyy/mm/dd USD 1000 | Stop by [Enter]")
+
 while 1:
     try:
         date,currency,money = map(str,input().split(' '))
@@ -11,6 +13,7 @@ while 1:
         for info in doc("tbody tr:nth-child(n)").items():
             if (info("td:nth-child(1).currency.phone-small-font > div > div.hidden-phone.print_show").text()[-4:-1]) == currency:
                 resultNTD += int(money)*float(info("td:nth-child(5)").text())
-    except EOFError:
+    except ValueError:
         break
-print(round(resultNTD))
+        
+print('Total Equal to ${} NTD.'.format(round(resultNTD)))
